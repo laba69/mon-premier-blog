e11b87866804effafcd36f401343ab8e7f36e1f0
Doudou <Doudou@HP-BALA.home>
2018-10-17 23:58:41 +0200

    c

--- mon-projet-angular/src/app/appareil/appareil.component.ts
+++ mon-projet-angular/src/app/appareil/appareil.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  @Input() appareilName: string;
+  @Input() appareilName: string; 
   @Input() appareilStatus: string;
   constructor() { }
 
