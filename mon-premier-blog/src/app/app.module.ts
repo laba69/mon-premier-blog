e4d1ce93ac620ea7df6dac26d6fbc17927c23e5d
Bala <Bala@bala.com>
2018-10-28 16:46:04 +0100

    v2

--- mon-premier-blog/src/app/app.module.ts
+++ mon-premier-blog/src/app/app.module.ts
@@ -5,24 +5,17 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { PostsService } from './services/posts.service';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from './services/auth-guard.service';
 import { PostFormComponent } from './post-form/post-form.component';
-import { SinglePostComponent } from './single-post/single-post.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './services/auth.service';
 import { ViewPostComponent } from './view-post/view-post.component';
 
 
 const appRoutes: Routes = [
-  { path: 'auth/signup', component: SignupComponent },
-  { path: 'auth/signin', component: SigninComponent },
-  { path: 'posts', canActivate: [AuthGuardService], component: PostListComponent },
-  { path: 'posts/new', canActivate: [AuthGuardService], component: PostFormComponent },
-  { path: 'posts/view/:id', canActivate: [AuthGuardService], component: SinglePostComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/new', component: PostFormComponent },
+  { path: 'posts/view/:id', component: ViewPostComponent },
   { path: '', redirectTo: 'posts', pathMatch: 'full' },
   { path: '**', redirectTo: 'posts' }
 ];
@@ -32,10 +25,7 @@ const appRoutes: Routes = [
     AppComponent,
     PostListComponent,
     PostListItemComponent,
-    SignupComponent,
-    SigninComponent,
     PostFormComponent,
-    SinglePostComponent,
     ViewPostComponent
   ],
   imports: [
@@ -46,9 +36,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    PostsService,
-    AuthService,
-    AuthGuardService
+    PostsService
   ],
   bootstrap: [AppComponent]
 })
